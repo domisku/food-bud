38b5e54337087c686c26911facb305799aea4ff7
Dominykas <dominykas.skuodziuna@visma.com>
2023-03-26 21:57:59 +0300

    Fix service worker

--- src/index.tsx
+++ src/index.tsx
@@ -13,16 +13,6 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   );
 }
 
-if ("serviceWorker" in navigator) {
-  try {
-    await navigator.serviceWorker.register("/sw.js", {
-      scope: "/",
-    });
-  } catch (error) {
-    console.error(`Service worker registration failed with ${error}`);
-  }
-}
-
 render(
   () => (
     <Router>
